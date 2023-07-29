34a6c789d98af2febd56483b76c36a7b09c5c6be
Samarth Salian <samarth.salian@i-exceed.com>
2023-07-29 21:58:09 +0530

    modied carousal

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-const ImageCarousel = ({ imageUrls, altText }) => {
+const ImageCarousel = ({ temple, altText }) => {
     return (
         <Carousel>
-            {imageUrls.map((url, index) => (
+            {temple.imageUrls.map((url, index) => (
                 <Carousel.Item key={index}>
                     <img className="d-block w-100" src={url} alt={altText} />
+                    <Carousel.Caption>
+                        <h4>{temple.name}</h4>
+                        <p>Deity: {temple.deity}</p>
+                    </Carousel.Caption>
                 </Carousel.Item>
             ))}
         </Carousel>
--- src/components/TempleDetails.js
+++ src/components/TempleDetails.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { Card, ListGroup, Badge } from "react-bootstrap";
-import { BsFillBrightnessHighFill } from "react-icons/bs";
+import { Card, ListGroup } from "react-bootstrap";
 import "./TempleDetails.css";
-import ImageCarousel from "./ImageCarousel"; // Assuming you put the ImageCarousel component in a separate file.
+import ImageCarousel from "./ImageCarousel";
 
 const TempleDetails = ({ temple }) => {
     return (
         <Card className="bg-dark text-white temple-details d-flex flex-row">
             <div style={{ maxWidth: "40%", width: "40%" }}>
-                {/* Use ImageCarousel instead of Card.Img */}
-                <ImageCarousel
-                    imageUrls={temple.imageUrls}
-                    altText={temple.name}
-                />
-                {/* Removed Card.ImgOverlay */}
-                <h4>
-                    <BsFillBrightnessHighFill /> {temple.name}
-                </h4>
-                <p>
-                    <strong>Deity:</strong> {temple.deity}
-                </p>
+                <ImageCarousel temple={temple} altText={temple.name} />
+                <div className="carousel-overlay">
+                    <h4>{temple.name}</h4>
+                    <p>
+                        <strong>Deity:</strong> {temple.deity}
+                    </p>
+                </div>
             </div>
             <div style={{ flexGrow: "2" }}>
                 <ListGroup variant="flush" className="temple-details-list">
