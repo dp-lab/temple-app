4b3bc7b10ce8b8dd4fa4de9a4ec523b3dbb32e0a
Durgaprasad <durgaprasad.salian.dp@gmail.com>
2023-07-29 22:54:50 +0530

    fix for blank dashboard

--- src/index.js
+++ src/index.js
@@ -8,4 +8,8 @@ import "./index.css";
 import App from "./App";
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
+//ReactDOM.render(<App />, document.getElementById("root"));
